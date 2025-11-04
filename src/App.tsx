4b49f5bf5e2a430198035ec76b65bef0483585bc
vakabunga <vakabunga@proton.me>
2025-11-04 14:53:46 +0300

    refactor: adds cistom hooks for geo and weather; adds the only one component with two hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { useCallback, useState } from 'react';
+import { WeatherWidget } from './components/WeatherWidget/WeatherWidget';
 
-import { MyGeo } from './components/MyGeo/MyGeo';
-import { Weather } from './components/Weather/Weather';
+import { cnApp } from './App.classname';
 
-export type Coords = {
-	latitude?: number;
-	longitude?: number
-}
+const App = () => {
 
-function App() {
-	const [coords, setCoords] = useState<Coords>({});
-
-	const handleGeopositioning = useCallback((pos: Coords) => {
-		setCoords({ latitude: pos.latitude, longitude: pos.longitude });
-	}, []);
 
 	return (
-		<div>
-			<MyGeo onGetGeo={handleGeopositioning} />
-			<Weather lon={coords.longitude} lat={coords.latitude} />
+		<div className={cnApp()}>
+			<WeatherWidget />
 		</div>
 	);
 }
